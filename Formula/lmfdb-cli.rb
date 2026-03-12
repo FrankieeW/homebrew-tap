e0a17bcacc0f96ca6c45eaad93fbc5320713a911
FrankieeW <git@frankie.wang>
2026-03-12 22:38:10 +0000

    Switch to Go version for Homebrew (simpler, no Python deps)

--- Formula/lmfdb-cli.rb
+++ Formula/lmfdb-cli.rb
@@ -1,31 +1,20 @@
-require "language/python"
-
 class LmfdbCli < Formula
   desc "CLI tool for querying LMFDB (L-Functions and Modular Forms Database)"
   homepage "https://github.com/FrankieeW/lmfdb-cli"
-  url "https://github.com/FrankieeW/lmfdb-cli.git", tag: "v1.2.0", revision: "ea827a5bd9277ffb3af6cd5a17acdded74a7f576"
+  url "https://github.com/FrankieeW/lmfdb-cli.git", tag: "v1.2.2", revision: "50d6448abcbebd1edc364d870e541ebd00b7131f"
   license "MIT"
   head "https://github.com/FrankieeW/lmfdb-cli.git", branch: "master"
 
-  depends_on "python@3.12"
+  depends_on "go" => :build
 
   def install
-    # Python package (with Playwright support for reCAPTCHA bypass)
-    virtualenv = Language::Python.virtualenv_create(prefix, "python3.12")
-    virtualenv.pip_install_and_link buildpath, "lmfdb-cli"
-
-    # Generate shell completions
-    bash_completion.install "#{lib}/python3.12/site-packages/lmfdb_cli/shell_completions/bash" => "lmfdb"
-    zsh_completion.install "#{lib}/python3.12/site-packages/lmfdb_cli/shell_completions/zsh" => "_lmfdb"
-    fish_completion.install "#{lib}/python3.12/site-packages/lmfdb_cli/shell_completions/fish" => "lmfdb.fish"
-  end
-
-  def post_install
-    # Install Playwright browsers after formula is installed
-    system "#{bin}/lmfdb", "install-browser"
+    # Build Go binary
+    ENV["CGO_ENABLED"] = "0"
+    system "go", "build", "-o", "lmfdb", "./cmd/lmfdb"
+    bin.install "lmfdb"
   end
 
   test do
-    assert_match "LMFDB CLI", shell_output("#{bin}/lmfdb --version")
+    assert_match "LMFDB CLI", shell_output("#{bin}/lmfdb version")
   end
 end
