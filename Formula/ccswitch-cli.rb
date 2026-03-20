d09bbf5aeba7a0f13ec1c4f28ac7d0a8c7b55066
Frankie Wang <git@frankie.wang>
2026-03-20 01:36:41 +0000

    feat: add ccswitch-cli formula

---
+++ Formula/ccswitch-cli.rb
@@ -0,0 +1,18 @@
+class CcswitchCli < Formula
+  desc "CLI for managing CC Switch providers"
+  homepage "https://github.com/FrankieeW/ccswitch-cli"
+  url "https://github.com/FrankieeW/ccswitch-cli.git", tag: "v0.1.0", revision: "9ccf714"
+  license "MIT"
+  head "https://github.com/FrankieeW/ccswitch-cli.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--release", "--manifest-path=Cargo.toml"
+    bin.install "target/release/ccswitch-cli" => "ccswitch-cli"
+  end
+
+  test do
+    assert_match "ccswitch-cli", shell_output("#{bin}/ccswitch-cli --version")
+  end
+end
