3c44aa07a3adc40e24c81bdaeaf8acda85eb7d93
FrankieeW <git@frankie.wang>
2026-06-07 01:54:15 +0100

    fix: pin leanmgr formula to v0.1.0 revision

--- Formula/leanmgr.rb
+++ Formula/leanmgr.rb
@@ -1,8 +1,9 @@
 class Leanmgr < Formula
   desc "Manage disposable .lake cache state across many Lean 4 projects"
   homepage "https://github.com/FrankieeW/leanmgr"
-  # NOTE: set `revision:` to the tagged commit SHA when v0.1.0 is pushed.
-  url "https://github.com/FrankieeW/leanmgr.git", tag: "v0.1.0"
+  url "https://github.com/FrankieeW/leanmgr.git",
+      tag:      "v0.1.0",
+      revision: "a037c2c4871076dc53e369be4be11e796bbaa4c7"
   license "MIT"
   head "https://github.com/FrankieeW/leanmgr.git", branch: "main"
 
